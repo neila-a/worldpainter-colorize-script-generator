7b1d5f5efc3d07767607ff081c9c16ba7a0e3758
Neil Alen <neilaspace@outlook.com>
2026-03-09 22:40:25 +0800

    🎨 使用空格缩进

--- src/colorizer.js
+++ src/colorizer.js
@@ -23,30 +23,30 @@ var terrainArr = _allPossibleBlocks_;
 
 var palette = [];
 
-var def = _def_ /* Array<"block r g b"> */
+var def = _def_; /* Array<"block r g b"> */
 
 print("\nUsing default palette");
 
 for (var i = 0; i < def.length; i++) {
-	var val = def[i].split(" ");
-	palette.push({ t: terrainArr.indexOf(val[0]), r: parseInt(val[1]), g: parseInt(val[2]), b: parseInt(val[3]) });
+    var val = def[i].split(" ");
+    palette.push({ t: terrainArr.indexOf(val[0]), r: parseInt(val[1]), g: parseInt(val[2]), b: parseInt(val[3]) });
 }
 
 var coloringMap = wp.getHeightMap().fromFile(arguments[0]).go();
 var mask = coloringMap;
 
 if (arguments.length > 2) {
-	for (var i = 2; i < arguments.length; i++) {
-		if (arguments[i] == "")
-			break;
+    for (var i = 2; i < arguments.length; i++) {
+        if (arguments[i] == "")
+            break;
 
-		if (arguments[i].indexOf("mask: ") == 0) {
-			mask = wp.getHeightMap().fromFile(arguments[i].substring(6).trim()).go();
+        if (arguments[i].indexOf("mask: ") == 0) {
+            mask = wp.getHeightMap().fromFile(arguments[i].substring(6).trim()).go();
 
-			print("\nMask detected");
-			print(arguments[i].substring(6).trim());
-		}
-	}
+            print("\nMask detected");
+            print(arguments[i].substring(6).trim());
+        }
+    }
 }
 
 var extent = coloringMap.getExtent();
@@ -56,51 +56,51 @@ print("Tiles that are empty will remain empty.");
 print("\nNow processing . . .");
 
 for (var x = extent.getX(); x < extent.getWidth(); x++) {
-	for (var y = extent.getY(); y < extent.getHeight(); y++) {
-		if (!dimension.isTilePresent(truncate((x + xDefault) / 128.0), truncate((y + yDefault) / 128.0)))
-			continue;
+    for (var y = extent.getY(); y < extent.getHeight(); y++) {
+        if (!dimension.isTilePresent(truncate((x + xDefault) / 128.0), truncate((y + yDefault) / 128.0)))
+            continue;
 
-		if (mask !== coloringMap && mask.getHeight(x, y) < 128)
-			continue;
+        if (mask !== coloringMap && mask.getHeight(x, y) < 128)
+            continue;
 
-		var color = new java.awt.Color(coloringMap.getColour(x, y), java.lang.Boolean.TRUE);
+        var color = new java.awt.Color(coloringMap.getColour(x, y), java.lang.Boolean.TRUE);
 
-		if (color.getAlpha() < 128)
-			continue;
+        if (color.getAlpha() < 128)
+            continue;
 
-		var distance = 999999;
-		var index = -1;
-		for (var i = 0; i < palette.length; i++) {
-			tempDistance = distanceBetween(color.getRed(), color.getGreen(), color.getBlue(), palette[i].r, palette[i].g, palette[i].b);
-			if (tempDistance < distance) {
-				distance = tempDistance;
-				index = i;
-			}
+        var distance = 999999;
+        var index = -1;
+        for (var i = 0; i < palette.length; i++) {
+            tempDistance = distanceBetween(color.getRed(), color.getGreen(), color.getBlue(), palette[i].r, palette[i].g, palette[i].b);
+            if (tempDistance < distance) {
+                distance = tempDistance;
+                index = i;
+            }
 
-			if (distance == 0)
-				break;
-		}
+            if (distance == 0)
+                break;
+        }
 
-		dimension.setTerrainAt(x + xDefault, y + yDefault, org.pepsoft.worldpainter.Terrain.VALUES[palette[index].t]);
-	}
+        dimension.setTerrainAt(x + xDefault, y + yDefault, org.pepsoft.worldpainter.Terrain.VALUES[palette[index].t]);
+    }
 }
 
 print("\nDone! :D");
 
 function pad(n) {
-	if (n < 10)
-		return "  " + n;
-	else if (n < 100)
-		return " " + n;
-	else
-		return "" + n;
+    if (n < 10)
+        return "  " + n;
+    else if (n < 100)
+        return " " + n;
+    else
+        return "" + n;
 }
 
 function truncate(number) {
-	return number > 0 ? Math.floor(number) : Math.ceil(number);
+    return number > 0 ? Math.floor(number) : Math.ceil(number);
 }
 
 //squared distance between two color values TODO: change to better color format
 function distanceBetween(r, g, b, rr, gg, bb) {
-	return (rr - r) * (rr - r) + (gg - g) * (gg - g) + (bb - b) * (bb - b);
+    return (rr - r) * (rr - r) + (gg - g) * (gg - g) + (bb - b) * (bb - b);
 }
